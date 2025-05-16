02f1318626fb667d01d30afe80a9a1ea9be19058
Denny Bucklin <dennis.bucklin@gmail.com>
2025-05-15 20:07:48 -0500

    settlements

---
+++ settlements/settlements.go
@@ -0,0 +1,14 @@
+package settlements
+
+type Settlement struct {
+	Name string
+	Purpose string
+	Size int
+	Atmosphere string
+	Government string
+	Amenities []string
+	Character string
+	Geography string
+}
+
+
